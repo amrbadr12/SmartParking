99b6eca182906aab98f641ebe0aa1af978df717d
amrbadr12 <amr.badr.9620@gmail.com>
2018-04-03 16:30:09 +0200

    second commit

--- app/src/main/java/com/smartparking/amrbadr12/smartparking/MainActivity.java
+++ app/src/main/java/com/smartparking/amrbadr12/smartparking/MainActivity.java
@@ -2,13 +2,20 @@ package com.smartparking.amrbadr12.smartparking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    //test
+    private Toolbar primaryToolbar;
+    private Toolbar cardviewToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        primaryToolbar= (Toolbar) findViewById(R.id.primary_toolbar);
+        cardviewToolbar=(Toolbar)findViewById(R.id.card_view_toolbar);
+        setSupportActionBar(cardviewToolbar);
+        getSupportActionBar().setTitle("Smart Parking");
     }
 }
